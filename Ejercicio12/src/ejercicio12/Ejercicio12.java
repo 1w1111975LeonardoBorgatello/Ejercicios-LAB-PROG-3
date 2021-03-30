9a7d2c0be3ca2d0cb3bf6f399076263e7eb22c7d
2w2111975LeonardoBorgatello <69583434+1w1111975LeonardoBorgatello@users.noreply.github.com>
2021-03-30 20:33:04 -0300

    Ejer 12 completo
    Se sube ejercicio 12 completo

--- Ejercicio12/src/ejercicio12/Ejercicio12.java
+++ Ejercicio12/src/ejercicio12/Ejercicio12.java
@@ -1,21 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ejercicio12;
 
-/**
- *
- * @author Cuis
- */
+import java.util.Scanner;
+
+
 public class Ejercicio12 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        int antiguedad;
+        int cTOTAL = 0;
+        double promedio = 0;
+        int cNuevo = 0;
+        int cPocouso = 0;
+        int cViejo = 0;
+        int cMuchouso = 0;
+        int acuNuevo = 0;
+        int acuPocouso = 0;
+        int acuMuchouso = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese la antiguedad del automovil, "
+                + "si desea finalizar ingrese 0: ");
+        antiguedad = sc.nextInt();
+        
+        while (antiguedad != 0){
+            if(antiguedad >= 1 && antiguedad <= 5){
+                acuNuevo += antiguedad;         
+                cNuevo++;
+                System.out.println("NUEVO");
+            }
+            if (antiguedad >= 6 && antiguedad <= 10){
+                cPocouso++;
+                acuPocouso += antiguedad;
+                System.out.println("POCO USO");
+            }
+            if (antiguedad >= 11 && antiguedad <= 20){
+                cMuchouso++;
+                acuMuchouso += antiguedad;
+                System.out.println("MUCHO USO");
+            }
+            if (antiguedad > 20){
+                cViejo++;
+                System.out.println("MUY ANTIGUO");
+            }            
+            
+            System.out.println("Ingrese la antiguedad del automovil, "
+                    + "si desea finalizar ingrese 0: ");
+            antiguedad = sc.nextInt();
+        
+        }           
+            
+        cTOTAL = cNuevo + cPocouso + cMuchouso + cViejo;
+        promedio = (double)(acuNuevo + acuPocouso + acuMuchouso) / 
+                (cNuevo + cPocouso + cMuchouso);
+        System.out.println("\nFinalizo la carga de vehiculos." + "\n" +
+                "La cantidad total de autos es: "+cTOTAL+ "\n"+
+                "La cantidad de vehiculos con poco uso es: "+cPocouso);
+        System.out.println("EL promedio de antiguedad de los vehiculos que no son"
+                + " muy antiguos es: " + promedio);
     }
     
 }
