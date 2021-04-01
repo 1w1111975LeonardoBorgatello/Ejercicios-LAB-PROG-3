596f559f376233c66a3cb2512852b0bdc9b62d32
Leonardo Borgatello <leonardo.borgatello@mercadolibre.com>
2021-04-01 14:20:40 -0300

    Se agrega un HashMap al ejercicio 11
    se agrega un hash

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+j
+:LABORATORIO 3/Ejercicio12/src/ejercicio12/Ejercicio12.java,f/b/fbc2f35107cb84576af7924270206051725188d8
+j
+:LABORATORIO 3/Ejercicio11/src/ejercicio11/Ejercicio11.java,e/e/ee874f683beb6001d38148f662a9ca07d5d5c70a
--- LABORATORIO 3/Ejercicio11/src/ejercicio11/Ejercicio11.java
+++ LABORATORIO 3/Ejercicio11/src/ejercicio11/Ejercicio11.java
@@ -3,6 +3,7 @@ package ejercicio11;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 //11. Ingresar el tiempo en segundos que realizó el ganador de la clasificación
 //de una carrera de F1. Luego ingresar los tiempos de los otros 9 corredores e
@@ -13,29 +14,33 @@ public class Ejercicio11 {
     public static void main(String[] args) {
         double tiempo1; 
         double tiempoN;
-        int corredores = 9;
+        String nombreCorredor;
+        int corredores = 3;
         int cont = 0;
-        ArrayList<Double> aCorredoresGral = new ArrayList();
+        //ArrayList<Double> aCorredoresGral = new ArrayList();
+        HashMap<String,Double> aCorredoresGral= new HashMap<>();
         ArrayList<Double> aCorredoresQparticipan = new ArrayList();
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese el tiempo del ganador: ");
         tiempo1 = sc.nextDouble();
         
         for (int i = 0; i < corredores; i++) {
-            System.out.println("Ingrese el tiempo del corredor:");
+            System.out.println("Ingrese el tiempo del corredor y luego su nombre:");
             tiempoN = sc.nextDouble();
+            nombreCorredor = sc.next();
             
             while (tiempoN < tiempo1){
                 System.out.println("El tiempo ingresado es menor al del ganador."
-                        + "No corresponde. Ingrese nuevamente el tiempo: ");
+                        + "No corresponde. Ingrese nuevamente el tiempo y su nombre: ");
                 tiempoN = sc.nextDouble();
-                
+                nombreCorredor = sc.next();
+
             }
             if (tiempoN > tiempo1 && tiempoN <= Ejercicio11.calculoTiempo(tiempo1)){
                 cont++;
                 aCorredoresQparticipan.add(tiempoN);
             } 
-            aCorredoresGral.add(tiempoN);
+            aCorredoresGral.put(nombreCorredor,tiempoN);
                     
         }
         System.out.println("Los corredores que pueden participar son: " + cont);
